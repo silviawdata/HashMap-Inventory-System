b647e5d31519e6e7892bcab372c22d5eccdea865
silviawdata <silviaw.data@gmail.com>
2026-05-24 23:54:47 -0700

    Update menuType.h

--- menuType.h
+++ menuType.h
@@ -30,28 +30,5 @@ public:
 
 
 
-/*NOTES ON HOW TO IMPLEMENT MAIN.CPP
-
-Our csv file, has 3 variables: name, price quantity.
-
-so basically parsing the file like in a previous code, i have done before. this is associted 
-with the menu type header file file. 
-
-parsing the file with comas. 
-
-can use the find function of the string. 
-can use strodfunction to convert a string into a decimal. remainder can be passed into an stoi function 
-then after each component, call the menu type constructor to initalize the object.
-then insert this object into the map using the name as the key. insert into the map one by one. 
-
-
-
-we can use a vector to store the items list. 
-
-
-
-
-
-
 
 */
